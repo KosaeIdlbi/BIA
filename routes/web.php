d948b9e2c2bed5d975e0b85c91480bdec0a1a1bb
Kosae <qusaiidlbi@gmail.com>
2026-04-26 18:06:19 +0300

    addfiles

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/health', function () {
     return response()->json(['status' => 'ok']);
 });
-
 Route::fallback(function () {
     return response("page not found");
 });
